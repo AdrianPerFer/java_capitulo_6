49c2f8541304eae871ba596c9c5f7f4234131200
Adrian <chopon007@gmail.com>
2022-10-30 21:07:37 +0100

    Ejercicio1 changed

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,6 +1,7 @@
 /*
  * 
- * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+ * Escribe un programa que muestre la tirada de tres dados. Se debe mostrar
+ * también la suma total (los puntos que suman entre los tres dados).
  * 
  * @author Adrián Perogil Fernández
  * 
