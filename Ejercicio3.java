897a80bf3b3f9a1ea8c6389c3711c4594ba882c2
Adrian <chopon007@gmail.com>
2022-10-31 11:44:04 +0100

    Ejercicio3 changed

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,3 +1,11 @@
+/*
+ * 
+ * Igual que el ejercicio anterior pero con la baraja española. Se utilizará la baraja
+ * de 40 cartas: 2, 3, 4, 5, 6, 7, sota, caballo, rey y as.
+ * 
+ * @author Adrián Perogil Fernández
+ * 
+ */
 public class Ejercicio3 {
     public static void main(String[] args) {
         String palo = "";
