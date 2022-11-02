f13192ffa611bc91cc42b64ffd426623d60aa6de
AdrianPerFer <chopon007@gmail.com>
2022-11-02 09:20:49 +0100

    Ejercicio11 changed

--- Ejercicio11.java
+++ Ejercicio11.java
@@ -3,7 +3,7 @@
  * Escribe un programa que muestre 20 notas generadas al azar. Las notas deben
  * aparecer de la forma: suspenso, suficiente, bien, notable o sobresaliente. Al
  * final aparecerá el número de suspensos, el número de suficientes, el número
- * de bienes, etc.
+ * de bienes, etc
  * 
  * @author Adrián Perogil Fernández
  * 
